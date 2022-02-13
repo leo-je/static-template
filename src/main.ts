90aed606a1c726b7f7c6908cc6f46c255af98867
te <shengteli@caih.com>
2022-02-14 00:59:23 +0800

    commit:add elementPlus

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import { appRouterContext } from './router'
 import store from './store'
 import App from './App.vue'
+import ElementPlus from 'element-plus';
 import 'ant-design-vue/dist/antd.css';
 
 createApp(App)
     .use(appRouterContext.routerState.router)
-    // .use(Antd)
+    .use(ElementPlus)
     .use(store)
     .mount('#app');
 
